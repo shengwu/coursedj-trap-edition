9fd6c6d37ff35bbeda86cda88c9cf8b4e3fb2ebc
Chris Yungmann <yungmann.chris@gmail.com>
2014-04-28 22:10:38 -0500

    Revert "Add and style welcome-modal with 1st time help"
    This reverts commit c141c69afc843442b6d42cc948d6d06b8e24b245.

--- js/styling.js
+++ js/styling.js
@@ -1,15 +1,11 @@
 $(document).ready(function(){
     $("#start").click(function(){
         $("#welcome").fadeOut(650);
-        $("#welcomeModal").modal();
     });
     $("#about").click(function(){
         $("#info").fadeIn(650); 
     });
     $("#back").click(function(){
         $("#info").fadeOut(650);   
-    });
-    $("#help").click(function(){
-    	$("#welcomeModal").modal();
     });
 });
